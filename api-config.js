b8d6ec41aa651043d59add5b72891c8d285b45fe
femiclassic11-ai <femiclassic11@gmail.com>
2026-05-26 14:27:39 +0100

    api update

--- api-config.js
+++ api-config.js
@@ -1,3 +1,2 @@
-// Set this to your deployed backend URL after SnapDeploy gives you one.
-// Example: window.CROSSED_CLASSIC_API_BASE = 'https://crossed-classic.snapdeploy.app';
+
 window.CROSSED_CLASSIC_API_BASE = 'https://crossed-classic-api.containers.snapdeploy.dev';
