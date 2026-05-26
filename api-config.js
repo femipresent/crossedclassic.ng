ba16e2df440fddee47a498328daa105dab5d8eb1
femiclassic11-ai <femiclassic11@gmail.com>
2026-05-26 13:00:27 +0100

    Initial commit: Crossed Classic website

---
+++ api-config.js
@@ -0,0 +1,3 @@
+// Set this to your deployed backend URL after SnapDeploy gives you one.
+// Example: window.CROSSED_CLASSIC_API_BASE = 'https://crossed-classic.snapdeploy.app';
+window.CROSSED_CLASSIC_API_BASE = '';
